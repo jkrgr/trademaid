f02ce1a08f49c80676143489e74914c71139d3e1
jk <jk@redpai.no>
2012-07-06 23:11:26 +0200

    stockgetter is a module that handels data gathering regarding stocks

---
+++ stockfinder/stockgetter.py
@@ -0,0 +1,14 @@
+#TODO: Implement function
+def find(ticker):
+    """
+    For a given 'ticker' this function returns a dict containing
+    {'ticker':'T', price:'P'}
+
+    (Can lookup towards yahoo finance or even better - use live data
+    from SEB )
+    """
+
+    d = {'ticker':ticker, 'price':'51.60'}
+    return d
+
+

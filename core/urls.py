de9a290e5581dbaca24eed90d899c0587c34c6ca
jk <jk@redpai.no>
2012-07-06 23:09:02 +0200

    core is the projects root folder
    The most general urls, views and templates will reside here.

---
+++ core/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin, admindocs
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    #url(r'^$', 'core.views.home', name='home'),
+    #url(r'^core/portfolio', include('trademaid.core.portfolio.urls')),
+    (r'^userpanel/', include('userpanel.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    #(r'^admin/doc/', include('admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', admin.site.urls),
+)

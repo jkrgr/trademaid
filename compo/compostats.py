2efffb84e1a0c03e78952e717ec3e77e1ccac9a7
jk <jk@redpai.no>
2012-07-06 23:31:54 +0200

    This module contains helper functions for the competition views

---
+++ compo/compostats.py
@@ -0,0 +1,27 @@
+"""
+This module contains helper fuctions for the compo view.
+"""
+
+def top_traders_today(n):
+    """
+    Renders HTML and displays the n best traders of today.
+    """
+    pass
+
+def top_traders_week(n):
+    """
+    Renders HTML and displays the n best traders of this week
+    """
+    pass
+
+def top_traders_month(n):
+    """
+    Renders HTML and displays the n best traders of this month
+    """
+    pass
+
+def top_traders_month(n):
+    """
+    Renders HTML and displays the n best traders of this year
+    """
+    pass

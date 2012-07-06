e1a61d23d4f526eccf28c7fba035c2e9147ce730
jk <jk@redpai.no>
2012-07-06 23:30:52 +0200

    The views will render the HTML-pages relating to the competition app

---
+++ compo/views.py
@@ -0,0 +1,11 @@
+"""
+Information and results regarding the competition is displayed here.
+"""
+
+import compostats
+
+def compo_view(request):
+    """
+    The page that will be displayed
+    """
+    pass

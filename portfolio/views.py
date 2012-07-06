6924154c405e6670ebf05a662b5b96261aa8fe96
jk <jk@redpai.no>
2012-07-06 23:16:31 +0200

    The views will render the html-pages that displays a users portfolio

---
+++ portfolio/views.py
@@ -0,0 +1,9 @@
+"""
+This module lets a trader view his stock portfolio.
+"""
+
+def portfolio_view(request):
+    '''
+    Renders an HTML-page where the user can view his portfolio
+    '''
+    pass

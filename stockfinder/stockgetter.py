c4c9d8a2c9bfcc639a4b70fb60e586a84ab72c71
Kåre Birger Lapstuen <lapstue@gmail.com>
2012-07-07 14:00:30 +0200

    Added code to fetch ticker prices from Yahoo Finance
    Code to fetch data from Yahoo Finance, based on ystockquote.py

--- stockfinder/stockgetter.py
+++ stockfinder/stockgetter.py
@@ -1,14 +1,14 @@
 #TODO: Implement function
 def find(ticker):
-    """
+    '''
     For a given 'ticker' this function returns a dict containing
     {'ticker':'T', price:'P'}
-
     (Can lookup towards yahoo finance or even better - use live data
     from SEB )
-    """
-
-    d = {'ticker':ticker, 'price':'51.60'}
-    return d
-
-
+    This version, a striped version of Corey Goldberg's ystockquote, fetches data from yahoo finance.
+    '''
+    import urllib
+    url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (ticker, 'l1')
+    price = urllib.urlopen(url).read().strip().strip('"')
+    dictionary = {'ticker': ticker, 'price': price}
+    return dictionary

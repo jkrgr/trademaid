0764fa7a45d3213f010586acbe0a51a86eb0ee72
Kåre Birger Lapstuen <lapstue@gmail.com>
2012-07-12 16:00:06 +0200

    Updated compo models to hold fields for stats
    The models file now has field to store user balance, and change in net
    worth for different time periods.

--- compo/models.py
+++ compo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+Class UserStats(models.Model):
+	user    = models.ForeignKey(User)
+	balance = models.DecimalField(max_digits=15, decimal_places=2)
+	#Fields to hold the change in net worth for different time periods
+	change_day = models.DecimalField(max_digits=15, decimal_places=2)
+	change_week = models.DecimalField(max_digits=15, decimal_places=2)
+	change_month = models.DecimalField(max_digits=15, decimal_places=2)
+	change_year = models.DecimalField(max_digits=15, decimal_places=2)

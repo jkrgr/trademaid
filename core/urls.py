7088fee782e58a2251fb43f159dd1c3f105251f6
Kåre Birger Lapstuen <lapstue@gmail.com>
2012-07-12 23:54:45 +0200

    Removed unused imports from core URL

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin, admindocs
-from django.contrib.auth.views import login, logout
 admin.autodiscover()
 
 urlpatterns = patterns('',

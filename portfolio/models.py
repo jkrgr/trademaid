361938f27d934ef55ff7b89ed57715507fd7483d
Stein-Olav <steinolavdavidsen@gmail.com>
2012-09-02 19:35:45 +0200

    added some more models in portfolio/models

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,17 +2,40 @@ from django.db import models
 from django.contrib.admin.models import User
 
 
+class StockQuote(models.Model)
+    id = models.AutoField(primary_key=True)
+    ticker = models.ForeignKey(Company)
+    quote = models.DecimalField(max_digits=15, decimal_places=2)
+    quotationDateTime = models.DateField(auto_now=True)
+	
+	def __init__(self, ticker, quote, quotationDateTime)
+	    self.ticker=ticker
+		self.quote=quote
+		self.quotationDateTime=quotationDateTime
+
+class Company(models.Model)
+    ticker = models.CharField(max_length=30)
+	name = models.CharField(max_length=255)
+	openForTrade = models.BooleanField()
+	
+	def __init__(self, ticker, name, openForTrade)
+	    self.ticker=ticker
+		self.name=name
+		self.openForTrade = openForTrade
+
 class Trade(models.Model):
     id      = models.AutoField(primary_key=True)
-    ticker  = models.CharField(max_length=255)
-    price   = models.DecimalField(max_digits=15, decimal_places=2)
-    buy     = models.BooleanField(default=True)
+	user    = models.ForeignKey(User)
+    ticker  = models.ForeignKey(Company)
+    quote_id   = models.ForeignKey(StockQuote)
+	tradeDateTime = models.DateField(auto_now=True)
+	volume = models.IntegerField()
     comment = models.TextField(blank=True)
-    user    = models.ForeignKey(User)
+    
 
-    def __init__(self, ticker, price, buy, comment, user):
-        self.ticker = ticker
-        self.price = price
-        self.buy = buy
-        self.comment = comment
+    def __init__(self, user, ticker, quote_id, volume, comment):
         self.user = user
+		self.ticker = ticker
+		self.quote_id = quote_id
+		self.volume = volume
+        self.comment = comment

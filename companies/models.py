db5e272cd89983f6b9bdf7f9803528bacff8777d
jk <jk@redpai.no>
2012-09-08 13:37:45 +0200

    Created a companies app that handles the companies

---
+++ companies/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.admin.models import User
+
+class Company(models.Model):
+    ticker = models.CharField(max_length=30)
+    name = models.CharField(max_length=255)
+    openForTrade = models.BooleanField()
+
+class CompanyAnalysis(models.Model):
+    id=models.AutoField(primary_key=True)
+    userID=models.ForeignKey(User)
+    company=models.ForeignKey(Company)
+    text=models.CharField(max_length=1000)
+    bulls=models.IntegerField()
+
+class StockQuote(models.Model):
+    id = models.AutoField(primary_key=True)
+    ticker = models.ForeignKey(Company)
+    quote = models.DecimalField(max_digits=15, decimal_places=2)
+    # When parsing the stocks, add correct date and time for last_trade
+    last_trade = models.DateField(blank=False)

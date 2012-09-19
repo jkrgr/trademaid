6c1de441000a021ef0551647b9d0d9cb22aac9fa
jk <jk@redpai.no>
2012-09-19 21:27:34 +0200

    cleaning up

--- companies/models.py
+++ companies/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.admin.models import User
 
+
 class Company(models.Model):
     ticker = models.CharField(max_length=30)
     name = models.CharField(max_length=255)
@@ -8,25 +9,25 @@ class Company(models.Model):
 
 
 class CompanyStatistics(models.Model):
-    id = models.AutoField(primary_key=True)
+    id      = models.AutoField(primary_key=True)
     company = models.ForeignKey(Company)
-    p_e = models.DecimalField(max_digits=15, decimal_places=2)
-    p_b = models.DecimalField(max_digits=15, decimal_places=2)
-    ebitda = models.DecimalField(max_digits=15, decimal_places=2)
+    p_e     = models.DecimalField(max_digits=15, decimal_places=2)
+    p_b     = models.DecimalField(max_digits=15, decimal_places=2)
+    ebitda  = models.DecimalField(max_digits=15, decimal_places=2)
     recordDateTime = models.DateField(auto_now=True)
 
 
 class CompanyAnalysis(models.Model):
-    id=models.AutoField(primary_key=True)
-    userID=models.ForeignKey(User)
-    company=models.ForeignKey(Company)
-    text=models.CharField(max_length=1000)
-    bulls=models.IntegerField()
+    id      = models.AutoField(primary_key=True)
+    userID  = models.ForeignKey(User)
+    company = models.ForeignKey(Company)
+    text    = models.CharField(max_length=1000)
+    bulls   = models.IntegerField()
 
 
 class StockQuote(models.Model):
-    id = models.AutoField(primary_key=True)
-    ticker = models.ForeignKey(Company)
-    quote = models.DecimalField(max_digits=15, decimal_places=2)
+    id      = models.AutoField(primary_key=True)
+    ticker  = models.ForeignKey(Company)
+    quote   = models.DecimalField(max_digits=15, decimal_places=2)
     # When parsing the stocks, add correct date and time for last_trade
     last_trade = models.DateField(blank=False)
